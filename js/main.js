a1e0f20add73d21ac0e2b8a00c8b9c2c23835df6
Mirko <mirko.longo.1987@gmail.com>
2020-02-27 14:55:07 +0100

    Inizio Compito

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// SCOPO DEL GIOCO: Intromettersi furtivamente nel codice scritto da un altro sviluppatore per aggiungere un effetto di js. In particolare l’attivazione del menu al click dell’hamburger. Nella repo trovate quindi il file HTML e il CSS compilati, quello che dovete scrivere voi è il JS.
+// Di seguito il testo dell’esercizio:
+// Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript.
